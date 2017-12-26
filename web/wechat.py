957d4c00bd59e9a81c0f507e7878363fd9423533
ban11111 <ban11111@qq.com>
2017-12-26 22:33:24 +0800

    Beta 版本，待添加微信功能。

--- web/wechat.py
+++ web/wechat.py
@@ -11,43 +11,47 @@ AppID = 'wx7945b4b4bcb70eae'
 
 @csrf_exempt
 def wechat(request):
-    signature = request.GET.get('signature', '')
-    timestamp = request.GET.get('timestamp', '')
-    nonce = request.GET.get('nonce', '')
-    echo_str = request.GET.get('echostr', '')
-    encrypt_type = request.GET.get('encrypt_type', '')
-    msg_signature = request.GET.get('msg_signature', '')
-    try:
-        check_signature(WECHAT_TOKEN, signature, timestamp, nonce)
-    except InvalidSignatureException:
-        echo_str = 'error'
     if request.method == 'GET':
+        signature = request.GET.get('signature', '')
+        timestamp = request.GET.get('timestamp', '')
+        nonce = request.GET.get('nonce', '')
+        echo_str = request.GET.get('echostr', '')
+        try:
+            check_signature(WECHAT_TOKEN, signature, timestamp, nonce)
+        except InvalidSignatureException:
+            echo_str = 'error'
         response = HttpResponse(echo_str, content_type="text/plain")
         return response
-    #elif request.method == 'POST':
-    #POST 请求
-    if encrypt_type == 'raw':
-        msg = parse_message(request.body)
-        response = HttpResponse(msg, content_type="application/xml")
-        return response
-    else:
-        from wechatpy.crypto import WeChatCrypto
-
-        crypto = WeChatCrypto(WECHAT_TOKEN, AES_Key, AppID)
-        try:
-            decrypted_xml = crypto.decrypt_message(
-                request.body,
-                msg_signature,
-                timestamp,
-                nonce
-            )
-        except (InvalidAppIdException, InvalidSignatureException):
-            # 处理异常或忽略
-            pass
-        msg = parse_message(decrypted_xml)
-        if msg.type == 'text':
-            reply = create_reply('这是条文字消息', msg)
+    elif request.method == 'POST':
+        timestamp = request.POST.get('timestamp', '')
+        nonce = request.POST.get('nonce', '')
+        encrypt_type = request.POST.get('encrypt_type', '')
+        msg_signature = request.POST.get('msg_signature', '')
+        if encrypt_type == 'raw':
+            msg = parse_message(request.body)
+            response = HttpResponse(msg, content_type="application/xml")
+            return response
         else:
-            reply = create_reply('这是其他类型消息', msg)
-        encrypted_xml = crypto.encrypt_message(reply.render(), nonce, timestamp)
-        return encrypted_xml
+            from wechatpy.crypto import WeChatCrypto
+
+            crypto = WeChatCrypto(WECHAT_TOKEN, AES_Key, AppID)
+            try:
+                decrypted_xml = crypto.decrypt_message(
+                    request.body,
+                    msg_signature,
+                    timestamp,
+                    nonce
+                )
+            except (InvalidAppIdException, InvalidSignatureException):
+                # 处理异常或忽略
+                pass
+            msg = parse_message(decrypted_xml)
+            if msg.type == 'text':
+                reply = create_reply('这是条文字消息', msg)
+            else:
+                reply = create_reply('这是其他类型消息', msg)
+            encrypted_xml = crypto.encrypt_message(reply.render(), nonce, timestamp)
+            return encrypted_xml
+
+    else:
+        return "nothing"
